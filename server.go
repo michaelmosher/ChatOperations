f8159bf6516d966fddace84abc5cba209e9adf0a
michaelmosher <mmosher47@gmail.com>
2017-02-06 20:47:05 -0500

    WIP database

--- server.go
+++ server.go
@@ -5,22 +5,30 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"database/sql"
 	"slackApi/api"
 )
+
+type Env struct {
+	db *sql.DB
+}
 
 func Index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Welcome!\n")
 }
 
 func main() {
+	// db, err := slackApi.NewDB(os.Getenv("DATABASE_URL"))
+	// if err != nil {
+	// 	log.Panic(err)
+	// }
+	// env := &Env{db: db}
+
 	port := os.Getenv("PORT")
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", Index)
 	mux.HandleFunc("/operations", slackApi.Operations)
 
-	// /oauth to start handshake
-	// /operations to do stuff
-
-	log.Fatal(http.ListenAndServe(":"+port, mux))
+	log.Fatal(http.ListenAndServe(":" + port, mux))
 }
