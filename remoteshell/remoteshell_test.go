e130b9394c279d4c0f787c084edb7656a2b56835
michaelmosher <mmosher47@gmail.com>
2017-02-19 18:08:55 -0500

    many fixes (handle it)

---
+++ remoteshell/remoteshell_test.go
@@ -0,0 +1,1 @@
+package remoteshell_test
--- slack/slack.go
+++ slack/slack.go
@@ -6,40 +6,32 @@ import (
 	"io"
 	"net/http"
 	"time"
+
+	"chatoperations/operations"
 )
 
 type Config struct {
-	WebhookUrl	string
-	NetClientTimeout  time.Duration
+	TemplatesGlob string
+	WebhookUrl    string
 }
 
 type Slack struct {
-	WebhookUrl        string
-	NetClient         *http.Client
-	JsonTemplates     *template.Template
+	WebhookUrl    string
+	NetClient     *http.Client
+	JsonTemplates *template.Template
 }
 
-type OperationsRequest struct {
-	Id           int64
-	Requester    string
-	Server       string
-	Action       string
-	Responder    string
-	Approved     bool
-	Response_url string
-}
-
-func New(cfg Config) Slack {
+func New(cfg Config) *Slack {
 	netClient := &http.Client{
-		Timeout: cfg.NetClientTimeout,
+		Timeout: time.Second * 10,
 	}
 	// load templates
-	templates := template.Must(template.ParseGlob("templates/*.json"))
+	templates := template.Must(template.ParseGlob(cfg.TemplatesGlob))
 
-	return Slack{
-		WebhookUrl:        cfg.WebhookUrl,
-		NetClient:         netClient,
-		JsonTemplates:     templates,
+	return &Slack{
+		WebhookUrl:    cfg.WebhookUrl,
+		NetClient:     netClient,
+		JsonTemplates: templates,
 	}
 }
 
@@ -62,23 +54,23 @@ func (slack *Slack) httpPost(url string, template_name string, data interface{})
 	return nil
 }
 
-func (slack *Slack) NotifyRequestSubmitted(opsRequest OperationsRequest) error {
-	return slack.httpPost(slack.WebhookUrl, "ops_request_submitted.json", opsRequest)
+func (slack *Slack) NotifyRequestSubmitted(o operations.Request) error {
+	return slack.httpPost(slack.WebhookUrl, "ops_request_submitted.json", o)
 }
 
-func (slack *Slack) NotifyRequestApproved(opsRequest OperationsRequest) error {
-	return slack.httpPost(opsRequest.Response_url, "request_approved.json", opsRequest)
+func (slack *Slack) NotifyRequestApproved(o operations.Request) error {
+	return slack.httpPost(o.Response_url, "request_approved.json", o)
 }
 
-func (slack *Slack) NotifyRequestRejected(opsRequest OperationsRequest) error {
-	return slack.httpPost(opsRequest.Response_url, "request_rejected.json", opsRequest)
+func (slack *Slack) NotifyRequestRejected(o operations.Request) error {
+	return slack.httpPost(o.Response_url, "request_rejected.json", o)
 }
 
 func (slack *Slack) NotifyError(url string, originalErr error) {
-	newError := slack.httpPost(url, "something_went_wrong.json", originalErr)
+	newError := slack.httpPost(url, "something_went_wrong.json", originalErr.Error)
 
 	if newError != nil {
 		fmt.Printf("Failed to send error notification: %s\nOriginal error: %s",
-					newError, originalErr)
+			newError, originalErr)
 	}
 }
